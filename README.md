b19940864a60a1234d1b73d90f19c487f54f7b88
staleas <sasund@gmail.com>
2024-08-08 12:00:01 +0200

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+
+### Install with stow:
+```bash
+stow --target .
+```
+
+### Homebrew installation:
+```bash
+# Leaving a machine
+brew leaves > leaves.txt
+
+# Fresh installation
+xargs brew install < leaves.txt
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,34 +1,18 @@
 local wezterm = require 'wezterm'
 return {
-	-- color_scheme = 'termnial.sexy',
 	color_scheme = 'Material (terminal.sexy)',
 	enable_tab_bar = true,
-	font_size = 16.0,
+	font_size = 12.0,
 	font = wezterm.font('JetBrains Mono'),
-	-- macos_window_background_blur = 40,
-	macos_window_background_blur = 30,
+	macos_window_background_blur = 10,
 	
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
-	-- window_background_opacity = 0.92,
-	window_background_opacity = 1.0,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
+	window_background_opacity = 0.97,
 	window_decorations = 'RESIZE',
 	keys = {
 		{
 			key = 'f',
 			mods = 'CTRL',
 			action = wezterm.action.ToggleFullScreen,
-		},
-		{
-			key = '\'',
-			mods = 'CTRL',
-			action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
 		},
 	},
 	mouse_bindings = {
