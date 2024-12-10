a24bf1529b30f9a995409d3494349888cf97f29b
Ståle Andreas Sund <sasund@gmail.com>
2024-12-10 13:54:56 +0100

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install --cask font-jetbrains-mono
 
 xargs brew install < homebrew/leaves.txt
 
+# Nix
+sh <(curl -L https://nixos.org/nix/install)
+
+# Devbox
 curl -fsSL https://get.jetify.com/devbox | bash
